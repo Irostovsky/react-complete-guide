bb1b4a7634b1aa037d436bd4bb7370db5b615f83
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2022-12-02 11:06:54 +0200

    Use arrow function style.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,12 +2,12 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   const rows = props.items.map((item) => (
     <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
   ));
 
   return <Card className="expenses">{rows}</Card>;
-}
+};
 
 export default Expenses;
