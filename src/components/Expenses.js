bc7b661ad3a95487a74fcf57b56e1b1d11be73f3
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2022-12-02 10:49:14 +0200

    Added a Card component.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,13 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function Expenses(props) {
   const rows = props.items.map((item) => (
     <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
   ));
 
-  return <div className="expenses">{rows}</div>;
+  return <Card className="expenses">{rows}</Card>;
 }
 
 export default Expenses;
