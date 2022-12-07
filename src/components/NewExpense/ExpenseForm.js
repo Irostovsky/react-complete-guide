68a76fe76dcb43e1b9fcc6089f9f5fef38af2833
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2022-12-07 16:14:58 +0200

    Back to solution without form wrapper.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -31,10 +31,6 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
-  const cancelFormHandler = () => {
-    props.onCancel();
-  };
-
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
@@ -68,7 +64,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button onClick={cancelFormHandler}>Cancel</button>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
